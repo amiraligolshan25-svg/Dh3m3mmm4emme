4f34f07048a33948af9901a7466e83fe3831d1e9
amiraligolshan25-svg <amiraligolshan25@gmail.com>
2026-08-18 07:47:58 +0330

    Update worldwar.py

--- worldwar.py
+++ worldwar.py
@@ -475,7 +475,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/blockade [کشور]\n"
         "/help"
     )
-    await update.message.reply_text(text, parse_mode="Markdown")
+    await update.message.reply_text(text)
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await start(update, context)
